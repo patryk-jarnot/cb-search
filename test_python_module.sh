0f2bac9c319c99e9371e504dbc099240e74466ee
MixedOne <MixedOne@example.com>
2025-07-30 20:06:01 +0200

    disabling python test

--- test_python_module.sh
+++ test_python_module.sh
@@ -1,28 +1,32 @@
-#!/usr/bin/env bash
+# #!/usr/bin/env bash
 
-set -e
-
-
-autoreconf -fi
-autoreconf -i
-
-rm -rf build
-
-mkdir build
-cd build
-
-INSTALLROOT=$PWD/../install-root
-
-rm -rf $INSTALLROOT
-
-../configure --prefix=$INSTALLROOT
-make
-make install
+# set -e
 
 
-# exact destination depends on python version
-# this is a hack; the idea is to get wherever "example.py" and "_example.so" are
-# onto PYTHONPATH
-export PYTHONPATH=$(dirname $(find $INSTALLROOT -name nscsearch.py -print))
-#python -c 'import example; example.example_hello()'
-python3 -c 'import nscsearch; nscsearch.example_hello()'
+# autoreconf -fi
+# autoreconf -i
+
+# rm -rf build
+
+# mkdir build
+# cd build
+
+# INSTALLROOT=$PWD/../install-root
+
+# rm -rf $INSTALLROOT
+
+# ../configure --prefix=$INSTALLROOT
+# make
+# make install
+
+
+# # exact destination depends on python version
+# # this is a hack; the idea is to get wherever "example.py" and "_example.so" are
+# # onto PYTHONPATH
+# export PYTHONPATH=$(dirname $(find $INSTALLROOT -name nscsearch.py -print))
+# #python -c 'import example; example.example_hello()'
+# python3 -c 'import nscsearch; nscsearch.example_hello()'
+
+
+
+# TODO
